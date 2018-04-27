2a8ba08cbd0ce40886d2405a8ed79b1cb6b04d7b
Marce212 <38596724+Marce212@users.noreply.github.com>
2018-04-27 11:26:33 -0300

    commit

---
+++ my_Blog_1.0/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using my_Blog_1._0.Models;
+
+namespace my_Blog_1._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //string titulo About
+        public IActionResult About()
+        {
+
+            ViewData["Message"] = "Bibliography";
+
+            return View();
+        }
+        //string Titulo Contact
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "About me.";
+
+            return View();
+        }
+        
+        public IActionResult InstallDockerAspNetCoreView()
+        {
+            ViewData["Message"] = "InstallDockerAspNetCoreView";
+
+            return View();
+        }
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+    }
+}
